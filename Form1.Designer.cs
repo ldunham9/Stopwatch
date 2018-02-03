b1781d0b1f939fa74cff1e8281c2c388d86178db
ldunham9 <35976476+ldunham9@users.noreply.github.com>
2018-02-03 12:50:40 -0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblStop = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblElapsed = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtStop = new System.Windows.Forms.TextBox();
+            this.txtElapsed = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(45, 52);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 38);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(45, 118);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(129, 38);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStart
+            // 
+            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(464, 52);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(150, 31);
+            this.lblStart.TabIndex = 2;
+            // 
+            // lblStop
+            // 
+            this.lblStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStop.Location = new System.Drawing.Point(464, 118);
+            this.lblStop.Name = "lblStop";
+            this.lblStop.Size = new System.Drawing.Size(150, 31);
+            this.lblStop.TabIndex = 3;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(45, 183);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(129, 38);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblElapsed
+            // 
+            this.lblElapsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblElapsed.Location = new System.Drawing.Point(464, 183);
+            this.lblElapsed.Name = "lblElapsed";
+            this.lblElapsed.Size = new System.Drawing.Size(150, 31);
+            this.lblElapsed.TabIndex = 5;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStart.Location = new System.Drawing.Point(254, 52);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(134, 29);
+            this.txtStart.TabIndex = 6;
+            this.txtStart.Text = "Start Time";
+            // 
+            // txtStop
+            // 
+            this.txtStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStop.Location = new System.Drawing.Point(254, 118);
+            this.txtStop.Name = "txtStop";
+            this.txtStop.Size = new System.Drawing.Size(134, 29);
+            this.txtStop.TabIndex = 7;
+            this.txtStop.Text = "End Time";
+            // 
+            // txtElapsed
+            // 
+            this.txtElapsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtElapsed.Location = new System.Drawing.Point(254, 183);
+            this.txtElapsed.Name = "txtElapsed";
+            this.txtElapsed.Size = new System.Drawing.Size(134, 29);
+            this.txtElapsed.TabIndex = 8;
+            this.txtElapsed.Text = "Elapsed Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 292);
+            this.Controls.Add(this.txtElapsed);
+            this.Controls.Add(this.txtStop);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.lblElapsed);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblStop);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblStop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblElapsed;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtStop;
+        private System.Windows.Forms.TextBox txtElapsed;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DateTime startTime;
+        DateTime endTime;
+        TimeSpan elapsedTime;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            startTime = DateTime.Now;
+            lblStart.Text = startTime.ToLongTimeString();
+            lblStop.Text = "";
+            lblElapsed.Text = "";
+            
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            endTime = DateTime.Now;
+            lblStop.Text = endTime.ToLongTimeString();
+            elapsedTime = endTime - startTime;
+            lblElapsed.Text = elapsedTime.Seconds.ToString();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            
+        }
+    }
+}
